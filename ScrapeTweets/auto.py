4f38360ff58107d685e448296cb17db9235fe4eb
keant1 <keant1@mcmaster.ca>
2021-03-07 20:24:16 -0500

    Resolved errors

--- ScrapeTweets/auto.py
+++ ScrapeTweets/auto.py
@@ -3,11 +3,11 @@ from scrape import *
 import schedule
 import time
 
-def schedule_query(hashtags, api, max_interval_query=450,freqency=15):
+def schedule_query(hashtags, api, max_interval_query=450):
     """ """
     n_queries = int(max_interval_query/len(hashtags))
     
-    print("Wake up to make {0} hashtag queries".format(n_queries))
+    print("Waking up to make {0} hashtag queries".format(n_queries))
     df_artem = query_hashtags(hashtags, artem, tweets_per_hashtag=n_queries)
     df_artem["urls"] = df_artem["text"].apply(regex_url)
 
@@ -17,6 +17,8 @@ def schedule_query(hashtags, api, max_interval_query=450,freqency=15):
     print("\n\n----\t----\tSaving images----\t----")
     for url in df_artem["image_urls"]:
         download_image(url)
+    print("----------------------------------------"
+    print("Downloads complete")
 
 
 if __name__ == '__main__':
@@ -32,11 +34,11 @@ if __name__ == '__main__':
 
     tags = get_hashtags("hashtags.txt")
 
-    n_mins = 15
+    n_mins = 1
 
     schedule_query(tags, artem)
     schedule.every(interval=n_mins).minutes.do( schedule_query, 
-                                                hashtags = tags, api = artem, frequency = n_mins)
+                                                hashtags = tags, api = artem)
 
     while True: 
         # Checks whether a scheduled task  
